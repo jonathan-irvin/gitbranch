e6bb4d7b32c6e38626c39c35a3f17a3e2ee4295b
Jonathan Irvin <irvinj@student.swosu.edu>
2015-10-02 10:27:06 -0500

    made program more friendly

--- src/GitBranch.java
+++ src/GitBranch.java
@@ -4,6 +4,8 @@ public class GitBranch {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello git");
+		
+		System.out.println("Today is a beautiful day.");
 
 	}
 

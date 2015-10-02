69a8e4e80cb34f5b6ca4cb5263d50786a562519b
Jonathan Irvin <irvinj@student.swosu.edu>
2015-10-02 10:22:40 -0500

    added hello git

--- src/GitBranch.java
+++ src/GitBranch.java
@@ -1,11 +1,9 @@
 
 public class GitBranch {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello git");
 
 	}
 
